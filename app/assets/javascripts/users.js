af57cc690427742973761b27fdafd0b049022a0e
ei-lene <eileneheng@gmail.com>
2013-03-10 19:29:21 -0400

    improve front-end validation feedback
    add keyup validation for password (weak, med, strong)
    add classes with styling for error messages

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -25,11 +25,11 @@ $(document).ready(function(){
   function validateUsername(){
     var userNameVal = userName.val();  
     if(userNameVal.length < 3) {
-      userNameError.show();
+      userNameError.show().addClass('red');
       return false;
     }
     else {
-      userNameError.hide();
+      userNameError.hide().addClass('red');
       return true;
     }
   }
@@ -38,7 +38,7 @@ $(document).ready(function(){
     var firstNameVal = firstName.val();
     var alphabet = /[a-zA-Z]/;
     if(firstNameVal.length < 3 || !alphabet.test(firstNameVal)) {
-      firstNameError.show();
+      firstNameError.show().addClass('red');
       return false;
     }
     else {
@@ -51,7 +51,7 @@ $(document).ready(function(){
     var lastNameVal = lastName.val();
     var alphabet = /[a-zA-Z]/;
     if(lastNameVal.length < 3 || !alphabet.test(lastNameVal)) {
-      lastNameError.show();
+      lastNameError.show().addClass('red');
       return false;
     }
     else {
@@ -64,11 +64,7 @@ $(document).ready(function(){
     var emailVal = email.val();
     var e = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/;
     if(!e.test(emailVal)) {
-      emailError.show();
-      return false;
-    }
-    else if(emailVal !== emailConfVal) {
-      emailConfError.show();
+      emailError.show().addClass('red');
       return false;
     }
     else {
@@ -81,11 +77,7 @@ $(document).ready(function(){
   function validatePassword(){
     var passwordVal = password.val();
     if(passwordVal.length < 6) {
-      passwordError.show();
-      return false;
-    }
-    else if(passwordVal !== passwordConfVal) {
-      passwordConfError.show();
+      passwordError.show().text("Password should have at least 6 characters").addClass('red');
       return false;
     }
     else {
@@ -95,26 +87,59 @@ $(document).ready(function(){
     }
   }
 
+  function validatePasswordStrength(){
+    var lowercase = /[a-z]/
+    var uppercase = /[A-Z]/
+    var numeric = /[0-9]/
+    var passwordVal = password.val();
+    passwordError.show(); 
+    if(lowercase.test(passwordVal) && uppercase.test(passwordVal) && numeric.test(passwordVal)){
+      
+      passwordError.text("Password strength: Strong").removeClass().addClass('green');
+    }
+    else if(lowercase.test(passwordVal) && uppercase.test(passwordVal)){
+      // passwordError.removeClass();
+      passwordError.text("Password strength: Medium").removeClass().addClass('amber');
+    }
+    else if(numeric.test(passwordVal) && uppercase.test(passwordVal)){
+      // passwordError.removeClass();
+      passwordError.text("Password strength: Medium").removeClass().addClass('amber');
+    }
+    else if(numeric.test(passwordVal) && lowercase.test(passwordVal)){
+      // passwordError.removeClass();
+      passwordError.text("Password strength: Medium").removeClass().addClass('amber');
+    }
+    else {
+      // passwordError.removeClass();
+      passwordError.text("Password strength: Weak").addClass('red');
+    }
+  }
+
   // Validate input formats on Blur
   userName.blur(function(){
     validateUsername(); 
   });
 
   firstName.blur(function(){
-     validateFirstname(); 
+    validateFirstname(); 
   });
 
   lastName.blur(function(){
-     validateLastname(); 
+    validateLastname(); 
   });
 
   email.blur(function(){
-     validateEmail(); 
+    validateEmail(); 
   });
 
   password.blur(function(){
-     validatePassword(); 
+    validatePassword(); 
   });
+
+  password.keyup(function(){
+     validatePasswordStrength(); 
+  });
+
 
   // Email confirmation validation on Keyup and Blur
   emailInputs.keyup(function(){
@@ -125,11 +150,12 @@ $(document).ready(function(){
         emailConfError.hide();
     }
     else if(emailVal === emailConfVal){
-      emailConfError.text("Email entries match")
+      emailConfError.text("Email entries match").addClass('green');
     }       
     else {
       emailConfError.hide();
-      emailConfError.text("Confirmation email does not match email")
+      emailConfError.removeClass;
+      emailConfError.text("Confirmation email does not match email").addClass('red');
       emailConfError.show();
     }
   }); 
@@ -142,7 +168,7 @@ $(document).ready(function(){
         emailConfError.hide();
     }
     else if(emailVal === emailConfVal){
-      emailConfError.text("Email entries match")
+      emailConfError.text("Email entries match").addClass('green');
     } 
   });
 
@@ -155,11 +181,13 @@ $(document).ready(function(){
         passwordConfError.hide();
     }
     else if(passwordVal === passwordConfVal){
-      passwordConfError.text("Password entries match")
+      passwordConfError.removeClass;
+      passwordConfError.text("Password entries match").addClass('green');
     }       
     else {
       passwordConfError.hide();
-      passwordConfError.text("Confirmation password does not match password")
+      passwordConfError.removeClass;
+      passwordConfError.text("Confirmation password does not match password").addClass('red');
       passwordConfError.show();
     }
   }); 
@@ -169,10 +197,10 @@ $(document).ready(function(){
     passwordVal = password.val();
     passwordConfError.show(); 
     if(passwordVal===''||passwordConfVal==='') {
-        passwordConfError.hide();
+      passwordConfError.hide();
     }
     else if(passwordVal === passwordConfVal){
-      passwordConfError.text("Password entries match")
+      passwordConfError.text("Password entries match").addClass('green');
     } 
   });  
 
